caab6cb3562db0045e6c5e43f2f09a54c8eb320c
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-27 21:31:10 +0530

    commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,13 +5,20 @@ public class Main {
 
         // Test case -1
 
-        Integer num1 = 10;
-        Integer num2 = 5;
-        Integer num3 = 3;
+        Integer intArr[] = { 3, 1, 35, 43, 5, 6 };
 
-        getMaximum<Integer> maxi = new getMaximum<Integer>(num1, num2, num3);
-        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi.find_max());
+        getMaximum<Integer> maxi1 = new getMaximum<Integer>(intArr);
+        System.out.println("The maximum of the given integer array is " + maxi1.find_max());
 
+        Float floatArr[] = { (float) 4.298, (float) 672.23, (float) 3.98, (float) 78.2 };
 
+        getMaximum<Float> maxi2 = new getMaximum<Float>(floatArr);
+        System.out.println("The maximum of the given float array is " + maxi2.find_max());
+
+    
+        String strArr[] = { "apple", "banana", "peanut", "peach", "mango", "lemon", "tomato" };
+
+        getMaximum<String> maxi3 = new getMaximum<String>(strArr);
+         System.out.println("The maximum of the given string array is " + maxi3.find_max());
     }
 }
