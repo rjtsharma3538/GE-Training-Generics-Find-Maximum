ca71931628607688f05f8b83ed0c2dcc2106a2a0
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-27 21:15:31 +0530

    commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,13 +5,30 @@ public class Main {
 
         // Test case -1
 
-        Integer num1 = 10;
-        Integer num2 = 5;
-        Integer num3 = 3;
+        String num1 = "peach";
+        String num2 = "apple";
+        String num3 = "banana";
 
-        getMaximum<Integer> maxi = new getMaximum<Integer>(num1, num2, num3);
-        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi.find_max());
+        getMaximum<String> maxi1 = new getMaximum<String>(num1, num2, num3);
+        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi1.find_max());
 
+        // Test case - 2
+
+        num1 = "apple";
+        num2 = "peach";
+        num3 = "banana";
+
+        getMaximum<String> maxi2 = new getMaximum<String>(num1, num2, num3);
+        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi2.find_max());
+
+
+        // Test Case - 3
+        num1 = "apple";
+        num2 = "banana";
+        num3 = "peach";
+
+        getMaximum<String> maxi3 = new getMaximum<String>(num1, num2, num3);
+        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi3.find_max());
 
     }
 }
