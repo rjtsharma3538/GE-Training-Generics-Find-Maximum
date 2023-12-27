53354945bb4b120541431a5d0474cea2fd3a30da
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-27 20:48:45 +0530

    commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,8 +9,26 @@ public class Main {
         Integer num2 = 5;
         Integer num3 = 3;
 
-        getMaximum<Integer> maxi = new getMaximum<Integer>(num1, num2, num3);
-        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi.find_max());
+        getMaximum<Integer> maxi1 = new getMaximum<Integer>(num1, num2, num3);
+        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi1.find_max());
+
+        // Test case -2 
+
+        num1 = 5;
+        num2 = 10;
+        num3 = 3;
+
+        getMaximum<Integer> maxi2 = new getMaximum<Integer>(num1, num2, num3);
+        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi2.find_max());
+
+        // Test case - 3
+
+        num1 = 5;
+        num2 = 3;
+        num3 = 10;
+
+        getMaximum<Integer> maxi3 = new getMaximum<Integer>(num1, num2, num3);
+        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi3.find_max());
 
 
     }
