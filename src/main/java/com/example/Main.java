0f220cae34d52157fbca6e499114f0d34cbe1ff3
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-27 21:36:34 +0530

    commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,13 +5,20 @@ public class Main {
 
         // Test case -1
 
-        Integer num1 = 10;
-        Integer num2 = 5;
-        Integer num3 = 3;
+        Integer intArr[] = { 3, 1, 35, 43, 5, 6 };
 
-        getMaximum<Integer> maxi = new getMaximum<Integer>(num1, num2, num3);
-        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi.find_max());
+        getMaximum<Integer> maxi1 = new getMaximum<Integer>(intArr);
+        maxi1.find_max();
 
+        Float floatArr[] = { (float) 4.298, (float) 672.23, (float) 3.98, (float) 78.2 };
 
+        getMaximum<Float> maxi2 = new getMaximum<Float>(floatArr);
+        maxi2.find_max();
+
+    
+        String strArr[] = { "apple", "banana", "peanut", "peach", "mango", "lemon", "tomato" };
+
+        getMaximum<String> maxi3 = new getMaximum<String>(strArr);
+        maxi3.find_max();
     }
 }
--- src/main/java/com/example/getMaximum.java
+++ src/main/java/com/example/getMaximum.java
@@ -1,28 +1,22 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class getMaximum<T extends Comparable<T>> {
 
-    T element1;
-    T element2;
-    T element3;
+    T arr[];
 
-    public getMaximum(T element1, T element2, T element3) {
-
-        this.element1 = element1;
-        this.element2 = element2;
-        this.element3 = element3;
+    public getMaximum(T arr[]) {
+        this.arr = arr;
     }
 
-    public T find_max() {
-        return getMax(element1, element2, element3);
+    public void find_max() {
+        getMax(this.arr);
     }
 
-    T getMax(T element1, T element2, T element3) {
-
-        if (element1.compareTo(element2) > 0 && element1.compareTo(element3) > 0)
-            return element1;
-        if (element2.compareTo(element3) > 0 && element2.compareTo(element1) > 0)
-            return element2;
-        return element3;
+    void getMax(T arr[]) {
+        Arrays.sort(this.arr);
+        
+        System.out.println("The maximum value among all elements of array is " + arr[arr.length - 1]);
     }
 }
