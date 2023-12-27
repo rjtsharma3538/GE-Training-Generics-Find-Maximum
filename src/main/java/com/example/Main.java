ae8552a5f27fc80ae706379e9faaab9c4bf35478
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-27 21:08:35 +0530

    commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,10 +7,28 @@ public class Main {
 
         Float num1 = (float)10.287;
         Float num2 = (float)5.378;
-        Float num3 = (float)10.2865;
+        Float num3 = (float)10.286;
 
         getMaximum<Float> maxi1 = new getMaximum<Float>(num1, num2, num3);
         System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi1.find_max());
+
+        // Test case - 2
+
+        num1 = (float)10.287;
+        num2 = (float)10.4;
+        num3 = (float)10.2865;
+
+        getMaximum<Float> maxi2 = new getMaximum<Float>(num1, num2, num3);
+        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi2.find_max());
+
+
+        // Test Case - 3
+        num1 = (float)10.287;
+        num2 = (float)5.378;
+        num3 = (float)10.2875;
+
+        getMaximum<Float> maxi3 = new getMaximum<Float>(num1, num2, num3);
+        System.out.println("The maximum of " + num1 +' ' + num2 + ' ' + num3 +" is "+ maxi3.find_max());
 
     }
 }
